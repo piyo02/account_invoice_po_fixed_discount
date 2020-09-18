f7c4feb285e327451121070eaa2385b3068dea31
piyo02 <alzidni2000@gmail.com>
2020-09-18 21:39:23 +0800

    push

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name': 'Template Addon',
+    'name': 'Account Invoice PO Fixed Discount',
     'author': 'technoindo.com',
     'category': 'hidden',
     'version': '10.0',
     'summary': 'Summary the addon.',
     'description': '''Description the addon'''
                    ,
-    'depends': ['base'],
+    'depends': ['account'],
     'data': [
-        ''
+        'views/account_invoice_view.xml'
     ],
     'images': [''],
     'auto_install': False,
